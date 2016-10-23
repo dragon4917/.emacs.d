2655c0e47506116ac67e76ebc4755577f16d7758
ice <dragon4917@126.com>
2016-10-23 15:08:46 +0800

    add the limited rules for submitting files and packages manager

--- customize-init.el
+++ customize-init.el
@@ -4,3 +4,9 @@
 ;; You may delete these explanatory comments.
 
 (package-initialize)
+	(add-to-list 'load-path "~/.emacs.d/packages/")
+	(require 'package)
+	(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
+	(add-to-list 'package-archives '("elpa" . "http://tromey.com/elpa/") t)
+	(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+	; (add-to-list 'package-archives '("melpa2" . "http://melpa.org/packages/") t)
